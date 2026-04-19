287e4bb6eaed6970ee6a8feb8720b38262950529
Fat Bird’s Owner <lykongvibol2012@gmail.com>
2026-04-19 15:20:29 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,4 @@
-function col(val){
-if (val >= 1.5){
-return "[sky]"
-} else if (val >= 2.5){
-return "[green]"
-} else if (val >= 3.5){
-return "[accent]"
-} else if (val >= 6.5){
-return "[red]"
-} else {
-return ""
-}}
+
 
 Events.on(ClientLoadEvent, () => {
     try{
@@ -37,13 +26,13 @@ Events.on(ClientLoadEvent, () => {
 
             slider.changed(() => {
                 speed = slider.getValue() + 1;
-                label.setText(col(speed) + speed.toFixed(2));
+                label.setText(speed.toFixed(2));
             });
 
             button.clicked(() => {
                 speed = 1;   
                 slider.setValue(0);
-                label.setText(col(speed) + speed.toFixed(2));
+                label.setText(speed.toFixed(2));
             });
 
             t.add(slider).width(150);
